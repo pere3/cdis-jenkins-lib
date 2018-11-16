d154e8a58ece8103703b4e95fee870426c5b169d
Ted Summer <ted.summer2@gmail.com>
2018-11-16 14:27:32 -0600

    debug(classes): test using microservice pipeline class

---
+++ src/uchicago/cdis/MicroservicePipeline.groovy
@@ -0,0 +1,34 @@
+package uchicago.cdis;
+
+import uchicago.cdis.KubeHelper;
+
+/**
+ * Groovy helper for deploying services to k8s in Jenkins pipelines.
+ *
+ * @see https://jenkins.io/doc/book/pipeline/shared-libraries/
+ */
+class MicroservicePipeline implements Serializable {
+  def steps
+  def kubeHelper
+  
+  /**
+   * Constructor
+   *
+   * @param steps injects hook to Jenkins Pipeline runtime
+   */
+  MicroservicePipeline(steps) {
+    this.steps = steps;
+    this.kubeHelper = new KubeHelper(steps)
+  }
+
+  /**
+   * Deploy the current env.GIT_BRANCH to the k8s
+   */
+  def execute() {
+    stage('Testing123') {
+      step {
+        this.kubeHelper.deployBranch('sheepdog')
+      }
+    }
+  }
+}
