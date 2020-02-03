4b93a8963079e3e08d00895bf16e829ea0832fca
Marcelo Costa <marceloc@uchicago.edu>
2020-02-03 16:16:32 -0600

    chore(qa): Run PR check against qa-dcp

--- vars/microservicePipeline.groovy
+++ vars/microservicePipeline.groovy
@@ -9,7 +9,7 @@ import org.jenkinsci.plugins.pipeline.modeldefinition.Utils
 */
 def call(Map config) {
   node {
-    def AVAILABLE_NAMESPACES = ['jenkins-blood', 'jenkins-brain', 'jenkins-niaid', 'jenkins-dcp', 'jenkins-genomel']
+    def AVAILABLE_NAMESPACES = ['qa-dcp']
     List<String> namespaces = []
     isDocumentationOnly = false
     prLabels = null
